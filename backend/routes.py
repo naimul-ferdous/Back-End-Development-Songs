d4bdf98be1988add78f4ca8ce666f303b3f0ffba
Naimul Ferdous <ferdousnaimul@gmail.com>
2024-01-21 13:48:43 -0500

    implemented songs service

--- backend/routes.py
+++ backend/routes.py
@@ -51,3 +51,88 @@ def parse_json(data):
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+
+@app.route("/health")
+def health():
+    return jsonify(dict(status="OK")), 200
+
+@app.route("/count")
+def count():
+    """return length of documents"""
+    count = db.songs.count_documents({})
+    return {"count": count}, 200
+
+
+@app.route("/song", methods=["GET"])
+def songs():
+    data= db.songs.find({})
+    if data:
+        return json_util.dumps({"songs":list(data)}), 200
+
+    return {"message": "Internal server error"}, 500
+
+
+@app.route("/song/<int:id>", methods=["GET"])
+def get_song_by_id(id):
+    song= db.songs.find_one({"id": id})
+    if song:
+        return json_util.dumps(song), 200
+    return {"message": f"song with {id} not found"}, 404
+
+
+@app.route("/song", methods=["POST"])
+def create_song():
+    body= request.json
+    id= body["id"];
+    song= db.songs.find_one({"id": id})
+
+    if song:
+        return jsonify({"Message":f"song with id {id} already present"}), 302
+    
+    insert_result= db.songs.insert_one(body)
+
+    return json_util.dumps({"inserted id": insert_result.inserted_id}), 201
+    
+
+@app.route("/song/<int:id>", methods=["PUT"])
+def update_song(id):
+    body= request.json
+    song= db.songs.find_one({"id": id})
+
+    if song:
+        # db.songs.update_one(body)
+
+        # Define a query to find the document you want to update
+        query = {"id": id}
+
+        # Define the update that you want to apply
+        update = {"$set": body}
+
+        # Update the first document matching the query
+        update_result = db.songs.update_one(query, update)
+
+        # Check if the update was successful
+        if update_result.modified_count > 0:
+            # Fetch the updated document
+            updated_document = db.songs.find_one(query)
+            # print("Updated document:", updated_document)
+            return json_util.dumps(updated_document), 200
+        else:
+            return jsonify({"message":"song found, but nothing updated"}), 200
+
+
+        
+    
+    return jsonify({"message":"song not found"}), 404
+
+
+@app.route("/song/<int:id>", methods=["DELETE"])
+def delete_song(id):
+    # Delete the first document matching the query
+    delete_result = db.songs.delete_one({"id": id})
+
+    # Check if the delete was successful
+    if delete_result.deleted_count > 0:
+        return jsonify(f'Documents deleted: {delete_result.deleted_count}'), 204
+    else:
+        return jsonify({"message":"song not found"}), 404
